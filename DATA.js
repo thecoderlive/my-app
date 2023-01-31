1b751c46bee0b7a8da45a63ae4672576a4fda895
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-01-31 17:17:47 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "549d7e5b-8dff-4ef6-9d2b-8e30568f48a6",
+    "home_feed": [
+        {
+            "id": "ab708e0a-d5a6-47f9-be15-cfe53149a818",
+            "artist_name": "far",
+            "home_carousel": [
+                {
+                    "id": "d4720654-e566-4d10-8c3c-aa15cfa8a9a4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/326.png"
+                },
+                {
+                    "id": "4eee9b71-bc8b-46ee-be68-0b556134bb54",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/308.png"
+                },
+                {
+                    "id": "4d43242e-00c0-42fd-83a7-43f8cab1710c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/79.png"
+                },
+                {
+                    "id": "af31f76c-7ec9-4555-a90a-45ba895fd9f3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/16.png"
+                },
+                {
+                    "id": "a9db2b83-51c0-457b-957a-9529e744dc8d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/252.png"
+                }
+            ],
+            "art_caption": "help",
+            "art_description": "Themselves letter teacher prove carry reach. Skin manager environmental."
+        },
+        {
+            "id": "7b37bc21-9d2e-41ad-89ce-ccad0d259865",
+            "artist_name": "each",
+            "home_carousel": [
+                {
+                    "id": "6131755d-3386-4d49-8e39-49a2975f70fd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/332.png"
+                },
+                {
+                    "id": "17f5c3b9-068f-4019-9888-8b8894b57425",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/22.png"
+                },
+                {
+                    "id": "82287668-21f5-4e74-8ba9-9eb11262d781",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/31.png"
+                },
+                {
+                    "id": "4058002e-3df3-489a-90bd-0e3a6f6740e2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/194.png"
+                },
+                {
+                    "id": "229dbfd4-3015-4ba0-8270-1be55ce9e01e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/10.png"
+                }
+            ],
+            "art_caption": "probably",
+            "art_description": "Question next her of sound sort most wait. Economic TV the fill quickly majority force world."
+        },
+        {
+            "id": "243275f8-9299-47ce-9ef8-0790d3a71d09",
+            "artist_name": "well",
+            "home_carousel": [
+                {
+                    "id": "2bdb674d-1864-4576-9e10-6eb414a4b846",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/58.png"
+                },
+                {
+                    "id": "6f5e024c-8442-4dd0-a9b5-2204d7ef765d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/265.png"
+                },
+                {
+                    "id": "e84599a3-779c-4174-bc33-d2e0f7dce952",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/128.png"
+                },
+                {
+                    "id": "4a19db87-5821-4c17-87d3-278490ca431c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/272.png"
+                },
+                {
+                    "id": "ffd9cb24-973f-4c4a-8c51-9216f2948593",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/64.png"
+                }
+            ],
+            "art_caption": "report",
+            "art_description": "Majority information list coach suffer nation check. Particular control hot suggest. Player PM well bar choice ground."
+        },
+        {
+            "id": "24daf6a3-0bb6-426f-9229-a67b68e31549",
+            "artist_name": "style",
+            "home_carousel": [
+                {
+                    "id": "7321a245-a1e5-4cdf-ba44-12bfa89aeab3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/125.png"
+                },
+                {
+                    "id": "3e312977-2dd8-4ebe-ba89-12f19d6c8368",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/110.png"
+                },
+                {
+                    "id": "c29b2390-d427-4b6c-a99e-e83064f8752b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/330.png"
+                },
+                {
+                    "id": "c67afedd-fbc9-4fc9-afd3-5bebd5aacb52",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/365.png"
+                },
+                {
+                    "id": "ca25873c-1280-4779-b2b9-a1462322a9f4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/130.png"
+                }
+            ],
+            "art_caption": "within",
+            "art_description": "Rest trip take young. Politics visit have evening kid. Everyone force thing star per community."
+        },
+        {
+            "id": "c849106a-30d3-4ec5-9b32-9ce444e39294",
+            "artist_name": "dog",
+            "home_carousel": [
+                {
+                    "id": "e880ffa9-b306-485a-9766-527a198eb395",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/90.png"
+                },
+                {
+                    "id": "384a240d-e454-493d-9c24-5ed83c7fbb25",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/206.png"
+                },
+                {
+                    "id": "2c20ac72-2bef-4020-9b18-b13138e429e5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/342.png"
+                },
+                {
+                    "id": "7cae3f33-ef0c-4f12-9fee-54187a7d5859",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/99.png"
+                },
+                {
+                    "id": "ea8cba26-7e11-4da5-9937-6c02a515a655",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/361.png"
+                }
+            ],
+            "art_caption": "international",
+            "art_description": "Stop phone space situation. What ago use watch market break. Boy add entire happen Republican seven present real."
+        }
+    ]
+}
